f4a14e99cfb21d86ee4809d36b14f2c360d6ff4c
teja-veera-karthikeya <105000597+teja-veera-karthikeya@users.noreply.github.com>
2022-09-23 19:37:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,3 +30,42 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
 }
+function check()
+{
+    img =document.getElementById("captured_img")
+    classifier.classify(img,gotResult)
+}    
+
+function gotResult(error,results){
+    if (error){
+        console.error();
+    }
+    else{
+        console.log(results)
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+
+        prediction_1=results[0].label;
+
+
+        speak();
+
+        if(results[0].label == "best"){
+            document.getElementById("update_emoji").innerHTML ="&#128076;";
+        }
+
+        if(results[0].label == "victory"){
+            document.getElementById("update_emoji").innerHTML =    "&#128077;";
+        }
+
+        if(results[0].label == "amzing"){
+            document.getElementById("update_emoji").innerHTML ="&#9996;";
+        }
+
+        
+    }
+
+
+}
+
+
+
